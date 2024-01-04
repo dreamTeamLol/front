ee9ddbb8d371f2b6c39941edc3b2ba9febef818a
mvodolagin <m.vodolagin@artsofte.ru>
2024-01-04 23:52:00 +0500

    login template

--- src/app/children/account/pages/layout-login/layout-login.page.ts
+++ src/app/children/account/pages/layout-login/layout-login.page.ts
@@ -1,4 +1,5 @@
-import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { Component, ChangeDetectionStrategy, inject, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './layout-login.page.html',
@@ -6,5 +7,38 @@ import { Component, ChangeDetectionStrategy } from "@angular/core";
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LayoutLoginPage {
+    public get languages(): string[] {
+        return this._languages;
+    }
 
+    public isChoosingLanguage: boolean = false;
+    public currentLanguage: string = '';
+
+    private _languages: string[] = [
+        'English (United States)',
+        'English (United Kingdom)',
+        'Русский (Россия)'
+    ]
+    private _router: Router = inject(Router);
+
+    @HostListener('click', ['$event'])
+    onClick(event: Event): void {
+        const target: Element = event.target as Element;
+        if (!target?.className.includes('languages')) {
+            this.isChoosingLanguage = false;
+        }
+    }
+
+    constructor() {
+        this.currentLanguage = this._languages[2];
+    }
+
+    public login(): void {
+        this._router.navigate(['/cabinet'])
+    }
+
+    public changeLanguage(newLanguage: string): void {
+        this.currentLanguage = newLanguage;
+        this.isChoosingLanguage = false;
+    }
 }
